a0e69de77edda47cf328a6cf416d5bd1bd9b7920
vinicius <vinicius.tecbcc@gmail.com>
2021-11-27 14:21:42 -0300

    comecando o cadastro de cervejas

--- src/main/java/br/com/vini/brewer/controller/CervejaController.java
+++ src/main/java/br/com/vini/brewer/controller/CervejaController.java
@@ -2,6 +2,9 @@ package br.com.vini.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.vini.brewer.model.Cerveja;
 
 @Controller
 public class CervejaController {
@@ -9,6 +12,14 @@ public class CervejaController {
 	@RequestMapping("/cerveja/novo")
 	public String novo() {
 		return "cerveja/cadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("SKU " + cerveja.getSku());
+		
+		return "cerveja/cadastroCerveja";
+		
 	}
 
 }
