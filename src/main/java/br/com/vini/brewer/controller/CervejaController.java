5bb7eb5347eafb364cf31596e2e6bd6abee6c6bf
vinicius <vinicius.tecbcc@gmail.com>
2021-11-27 12:47:46 -0300

    configurando o view resolver com thymeleaf

--- src/main/java/br/com/vini/brewer/controller/CervejaController.java
+++ src/main/java/br/com/vini/brewer/controller/CervejaController.java
@@ -1,8 +1,14 @@
 package br.com.vini.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejaController {
+	
+	@RequestMapping("/cerveja/novo")
+	public String novo() {
+		return "cerveja/cadastroCerveja";
+	}
 
 }
