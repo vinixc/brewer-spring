81f7f57b102be47c84fb62743f0e660d762030fc
vinicius <vinicius.tecbcc@gmail.com>
2021-11-28 16:21:14 -0300

    adicionando mais campos e validacao

--- src/main/java/br/com/vini/brewer/controller/CervejaController.java
+++ src/main/java/br/com/vini/brewer/controller/CervejaController.java
@@ -21,12 +21,13 @@ public class CervejaController {
 	
 	@RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		
 		if(result.hasErrors()) {
 			model.addAttribute("mensagem","Erro no formulario");
 			return "cerveja/cadastroCerveja";
 		}
 		
-		System.out.println("SKU " + cerveja.getSku());
+		System.out.println("SKU " + cerveja.toString());
 		attributes.addFlashAttribute("mensagem","Cerveja salva com sucesso!");
 		
 		return "redirect:/cerveja/novo";
--- src/main/java/br/com/vini/brewer/model/Cerveja.java
+++ src/main/java/br/com/vini/brewer/model/Cerveja.java
@@ -1,12 +1,19 @@
 package br.com.vini.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	
 	@NotBlank
 	private String sku;
+	
+	@NotBlank
 	private String nome;
+	
+	@NotBlank @Size(max = 50)
+	private String descricao;
 	
 	public String getSku() {
 		return sku;
@@ -19,5 +26,15 @@ public class Cerveja {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public String toString() {
+		return "Cerveja [sku=" + sku + ", nome=" + nome + ", descricao=" + descricao + "]";
 	}
 }
