20b66b8b209861d7d1d2975963d3dad5f986b190
vinicius <vinicius.tecbcc@gmail.com>
2021-12-07 17:59:23 -0300

    montando os combos e radios do cadastro de cerveja

--- src/main/java/br/com/vini/brewer/controller/CervejaController.java
+++ src/main/java/br/com/vini/brewer/controller/CervejaController.java
@@ -10,10 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.vini.brewer.model.Cerveja;
+import br.com.vini.brewer.model.Origem;
+import br.com.vini.brewer.model.Sabor;
 import br.com.vini.brewer.repository.CervejaRepository;
+import br.com.vini.brewer.repository.EstiloRepository;
 
 @Controller
 public class CervejaController {
@@ -23,13 +27,21 @@ public class CervejaController {
 	@Autowired
 	private CervejaRepository cervejaRepository;
 	
+	@Autowired
+	private EstiloRepository estiloRepository;
+	
 	@RequestMapping("/cerveja/novo")
-	public String novo(Cerveja cerveja) {
-		return "cerveja/cadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/cadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estiloRepository.findAll());
+		mv.addObject("origens", Origem.values());
+		
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if(result.hasErrors()) {
 			return novo(cerveja);
@@ -38,6 +50,6 @@ public class CervejaController {
 		System.out.println("SKU " + cerveja.toString());
 		attributes.addFlashAttribute("mensagem","Cerveja salva com sucesso!");
 		
-		return "redirect:/cerveja/novo";
+		return new ModelAndView("redirect:/cerveja/novo");
 	}
 }
---
+++ src/main/java/br/com/vini/brewer/repository/EstiloRepository.java
@@ -0,0 +1,9 @@
+package br.com.vini.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.vini.brewer.model.Estilo;
+
+public interface EstiloRepository extends JpaRepository<Estilo, Long>{
+
+}
