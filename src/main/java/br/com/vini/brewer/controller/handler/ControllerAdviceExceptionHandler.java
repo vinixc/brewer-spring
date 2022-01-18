f13b99765d5fb37bc5756214a656a724491401e8
vinicius <vinicius.tecbcc@gmail.com>
2022-01-18 18:36:11 -0300

    Tratando exceções com ExceptionHandler

---
+++ src/main/java/br/com/vini/brewer/controller/handler/ControllerAdviceExceptionHandler.java
@@ -0,0 +1,16 @@
+package br.com.vini.brewer.controller.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.vini.brewer.exception.NomeEstiloJaCadastradoException;
+
+@ControllerAdvice
+public class ControllerAdviceExceptionHandler {
+	
+	@ExceptionHandler(NomeEstiloJaCadastradoException.class)
+	public ResponseEntity<String> handleNomeEstiloJaCadastradoException(NomeEstiloJaCadastradoException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+}
