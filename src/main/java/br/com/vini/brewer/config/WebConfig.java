2beefbeac4f5a329777726385eac2d1c45aa1c7c
vinicius <vinicius.tecbcc@gmail.com>
2022-01-19 12:12:05 -0300

    Novo atributo para classe de erro e dialect com thymeleaf

--- src/main/java/br/com/vini/brewer/config/WebConfig.java
+++ src/main/java/br/com/vini/brewer/config/WebConfig.java
@@ -27,6 +27,7 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import br.com.vini.brewer.controller.CervejaController;
 import br.com.vini.brewer.controller.converter.EstiloConverter;
+import br.com.vini.brewer.thymeleaf.BrewerDialect;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @Configuration
@@ -56,6 +57,7 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		engine.setTemplateResolver(templateResolver());
 		
 		engine.addDialect(new LayoutDialect());
+		engine.addDialect(new BrewerDialect());
 		return engine;
 	}
 
---
+++ src/main/java/br/com/vini/brewer/thymeleaf/BrewerDialect.java
@@ -0,0 +1,25 @@
+package br.com.vini.brewer.thymeleaf;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.thymeleaf.dialect.AbstractProcessorDialect;
+import org.thymeleaf.processor.IProcessor;
+import org.thymeleaf.standard.StandardDialect;
+
+import br.com.vini.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+
+public class BrewerDialect extends AbstractProcessorDialect{
+	
+	public BrewerDialect() {
+		super("Brewer", "brewer", StandardDialect.PROCESSOR_PRECEDENCE);
+	}
+
+	@Override
+	public Set<IProcessor> getProcessors(String dialectPrefix) {
+		final Set<IProcessor> processadores = new HashSet<>();
+		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
+		return processadores;
+	}
+
+}
