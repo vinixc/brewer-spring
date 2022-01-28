71350f24d025ff2b9e9cb1a874c4f92a5b7d2535
vinicius <vinicius.tecbcc@gmail.com>
2022-01-28 17:41:46 -0300

    Mostrando total de páginas dinamicamente

--- src/main/java/br/com/vini/brewer/repository/helper/cerveja/CervejaRepositoryImpl.java
+++ src/main/java/br/com/vini/brewer/repository/helper/cerveja/CervejaRepositoryImpl.java
@@ -1,14 +1,15 @@
 package br.com.vini.brewer.repository.helper.cerveja;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -24,7 +25,7 @@ public class CervejaRepositoryImpl implements CervejaRepositoryQuery{
 	@Override
 	@Transactional(readOnly = true)
 	@SuppressWarnings("unchecked")
-	public List<Cerveja> filtrar(CervejaFilter filter, Pageable pageable) {
+	public Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
 		
 		int totalRegistersForpage = pageable.getPageSize();
@@ -34,43 +35,57 @@ public class CervejaRepositoryImpl implements CervejaRepositoryQuery{
 		criteria.setFirstResult(firstRegister);
 		criteria.setMaxResults(totalRegistersForpage);
 		
-		
 		if(filter != null) {
-			
-			if(!StringUtils.isEmpty(filter.getSku())) {
-				criteria.add(Restrictions.ilike("sku",  "%" + filter.getSku() + "%"));
-			}
-			
-			if(!StringUtils.isEmpty(filter.getNome())) {
-				criteria.add(Restrictions.ilike("nome",filter.getNome(), MatchMode.ANYWHERE));
-			}
-			
-			if(isEstiloPresent(filter)) {
-				criteria.add(Restrictions.eq("estilo", filter.getEstilo()));
-			}
-			
-			if(filter.getSabor() != null) {
-				criteria.add(Restrictions.eq("sabor", filter.getSabor()));
-			}
-			
-			if(filter.getOrigem() != null) {
-				criteria.add(Restrictions.eq("origem", filter.getOrigem()));
-			}
-			
-			if(filter.getValorDe() != null) {
-				criteria.add(Restrictions.ge("valor", filter.getValorDe()));
-			}
-			
-			if(filter.getValorAte() != null) {
-				criteria.add(Restrictions.le("valor", filter.getValorAte()));
-			}
+			adicionaRestricoes(filter, criteria);
 		}
 		
-		return criteria.list();
+		return new PageImpl<>(criteria.list(), pageable, total(filter));
+	}
+
+	private Long total(CervejaFilter filter) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+		
+		if(filter != null) {
+			adicionaRestricoes(filter, criteria);
+		}
+		
+		criteria.setProjection(Projections.rowCount());
+		
+		return (Long) criteria.uniqueResult();
 	}
 
 	private boolean isEstiloPresent(CervejaFilter filter) {
 		return filter.getEstilo() != null && filter.getEstilo().getId() != null;
+	}
+	
+	private void adicionaRestricoes(CervejaFilter filter, Criteria criteria) {
+		if(!StringUtils.isEmpty(filter.getSku())) {
+			criteria.add(Restrictions.ilike("sku",  "%" + filter.getSku() + "%"));
+		}
+		
+		if(!StringUtils.isEmpty(filter.getNome())) {
+			criteria.add(Restrictions.ilike("nome",filter.getNome(), MatchMode.ANYWHERE));
+		}
+		
+		if(isEstiloPresent(filter)) {
+			criteria.add(Restrictions.eq("estilo", filter.getEstilo()));
+		}
+		
+		if(filter.getSabor() != null) {
+			criteria.add(Restrictions.eq("sabor", filter.getSabor()));
+		}
+		
+		if(filter.getOrigem() != null) {
+			criteria.add(Restrictions.eq("origem", filter.getOrigem()));
+		}
+		
+		if(filter.getValorDe() != null) {
+			criteria.add(Restrictions.ge("valor", filter.getValorDe()));
+		}
+		
+		if(filter.getValorAte() != null) {
+			criteria.add(Restrictions.le("valor", filter.getValorAte()));
+		}
 	}
 
 }
--- src/main/java/br/com/vini/brewer/repository/helper/cerveja/CervejaRepositoryQuery.java
+++ src/main/java/br/com/vini/brewer/repository/helper/cerveja/CervejaRepositoryQuery.java
@@ -1,7 +1,6 @@
 package br.com.vini.brewer.repository.helper.cerveja;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import br.com.vini.brewer.model.Cerveja;
@@ -9,6 +8,6 @@ import br.com.vini.brewer.repository.filter.CervejaFilter;
 
 public interface CervejaRepositoryQuery {
 	
-	public List<Cerveja> filtrar(CervejaFilter filter, Pageable pageable);
+	public Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable);
 
 }
