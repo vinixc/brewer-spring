69a48404eeedc2806077e47d2342beaa279cd84d
vinicius <vinicius.tecbcc@gmail.com>
2022-03-03 18:10:56 -0300

    populando estados no cadastro de cliente

--- src/main/java/br/com/vini/brewer/controller/ClienteController.java
+++ src/main/java/br/com/vini/brewer/controller/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.vini.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,15 +13,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.vini.brewer.model.Cliente;
 import br.com.vini.brewer.model.TipoPessoa;
+import br.com.vini.brewer.repository.EstadoRepository;
 
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController {
 	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Cliente cliente) {
 		ModelAndView mv = new ModelAndView("cliente/cadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estadoRepository.findAll());
 		
 		return mv;
 	}
