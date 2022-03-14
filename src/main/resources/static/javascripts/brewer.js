d5299de9defff9d150bb227d2868599df561b722
vinicius <vinicius.tecbcc@gmail.com>
2022-03-14 20:36:35 -0300

    configurando o csrf no projeto

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -83,6 +83,23 @@ Brewer.MaskDate = (function(){
 	return MaskDate;
 }());
 
+Brewer.Security = (function(){
+		
+	function Security(){
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header]').val();	
+	}
+	
+	Security.prototype.enable = function(){
+		$(document).ajaxSend(function(event, jqxhr,settings){
+			jqxhr.setRequestHeader(this.header,this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+	
+}());
+
 $(function(){
 	
 	var maskMoney = new Brewer.MaskMoney();
@@ -97,4 +114,6 @@ $(function(){
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
 	
+	var security = new Brewer.Security();
+	security.enable();
 })
