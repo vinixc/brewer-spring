ced7c2100bc1c4ae8de31e808c8991390b3b8f7f
vinicius <vinicius.tecbcc@gmail.com>
2022-03-15 21:29:30 -0300

    Implementando menu lateral

--- src/main/java/br/com/vini/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/br/com/vini/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import br.com.vini.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import br.com.vini.brewer.thymeleaf.processor.MenuAttibuteTagProcessor;
 import br.com.vini.brewer.thymeleaf.processor.MessageElementTagProcessor;
 import br.com.vini.brewer.thymeleaf.processor.OrderElementTagProcessor;
 import br.com.vini.brewer.thymeleaf.processor.PaginationElementTagProcessor;
@@ -25,6 +26,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
+		processadores.add(new MenuAttibuteTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
