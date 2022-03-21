e4dce8c43f38ac92f0d98992c4dcc309fae1a86a
vinicius <vinicius.tecbcc@gmail.com>
2022-03-21 19:55:00 -0300

    Testando a tabela de itens de venda

--- src/main/java/br/com/vini/brewer/venda/TabelaItensVenda.java
+++ src/main/java/br/com/vini/brewer/venda/TabelaItensVenda.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.vini.brewer.model.Cerveja;
 import br.com.vini.brewer.model.ItemVenda;
 
 public class TabelaItensVenda {
@@ -14,6 +15,15 @@ public class TabelaItensVenda {
 		return itens.stream()
 			.map(ItemVenda::getValorTotal)
 			.reduce(new BigDecimal(0), (i1,i2) -> i1.add(i2));
+	}
+	
+	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setCerveja(cerveja);
+		itemVenda.setQuantidade(quantidade);
+		itemVenda.setValorUnitario(cerveja.getValor());
+		
+		itens.add(itemVenda);
 	}
 				
 	public List<ItemVenda> getItens() {
